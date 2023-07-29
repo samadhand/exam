9e215d05fee7e59d3fa8ef41f5cf0ddc74b0d312
samadhan <samadhan123>
2023-07-29 10:20:59 +0530

    New Exam

---
+++ server.py
@@ -0,0 +1,16 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET"])
+def root():
+    return "Welcome TO ITIL exam"
+
+@app.route("/modules", methods=["GET"])
+def root1():
+    return "FCN, Security Concepts, COSA, NDC, PKI, CF, CA, ITIL&DevOps"
+@app.route("/me", methods=["GET"])
+def root2():
+    return "230344223042 Samadhan 7007007007"
+
+app.run(host="0.0.0.0", port=4000, debug=True)
